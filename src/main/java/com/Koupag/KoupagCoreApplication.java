300daa7caaac0019fb0171590d2df1fdb5bb959e
Imdad Rind <aimdad79@hotmail.com>
2023-06-19 22:46:51 +0500

    intital setup

---
+++ src/main/java/com/Koupag/KoupagCoreApplication.java
@@ -0,0 +1,13 @@
+package com.Koupag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoupagCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoupagCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Koupag/KoupagCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Koupag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoupagCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
